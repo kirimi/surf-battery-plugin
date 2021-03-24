07ec0f24cacec66f008ca4dd01d3bda88f9cbbc4
Kirill Mironov <kirill.mironov@gmail.com>
2021-03-24 18:52:40 +0300

    initial commit

---
+++ example/android/app/src/main/kotlin/ru/kirimi/battery/battery_example/MainActivity.kt
@@ -0,0 +1,6 @@
+package ru.kirimi.battery.battery_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/battery.dart
@@ -0,0 +1,14 @@
+
+import 'dart:async';
+
+import 'package:flutter/services.dart';
+
+class Battery {
+  static const MethodChannel _channel =
+      const MethodChannel('battery');
+
+  static Future<String> get platformVersion async {
+    final String version = await _channel.invokeMethod('getPlatformVersion');
+    return version;
+  }
+}
